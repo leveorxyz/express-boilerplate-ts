08357b7a7931cf129d3e63c843d1847aa59ee6e3
sihan-tawsik <sihantawsik@gmail.com>
2022-10-10 23:07:07 +0600

    Update auth factory return type

--- auth/index.ts
+++ auth/index.ts
@@ -1,3 +1,7 @@
+import { Request, Response, Router, NextFunction } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import QueryString from "qs";
+
 import { httpRouters, httpMiddleware } from "./http";
 import { jwtRouters, jwtMiddleware } from "./jwt";
 
@@ -6,10 +10,25 @@ export enum AuthSchemes {
   HTTP = "http",
 }
 
+type authMapKey = {
+  middleware: (
+    req: Request<
+      ParamsDictionary,
+      any,
+      any,
+      QueryString.ParsedQs,
+      Record<string, any>
+    >,
+    res: Response<unknown>,
+    next: NextFunction
+  ) => Promise<unknown>;
+  router: Router;
+};
+
 export const authFactory = (
   scheme: typeof AuthSchemes[keyof typeof AuthSchemes]
 ) => {
-  const authMap = new Map<string, Object>([
+  const authMap = new Map<string, authMapKey>([
     [AuthSchemes.JWT, { middleware: jwtMiddleware, router: jwtRouters }],
     [AuthSchemes.HTTP, { middleware: httpMiddleware, router: httpRouters }],
   ]);
@@ -18,7 +37,9 @@ export const authFactory = (
 
   if (!authProduct) {
     throw new Error(
-      `Invalid auth scheme. Must be any of ${Object.values(AuthSchemes)}`
+      `Invalid auth scheme. Must be any of ${JSON.stringify(
+        Object.values(AuthSchemes)
+      )}`
     );
   }
 
